410885ed14d2fdcc71a3ec4ded116d58b1489aed
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-10-31 10:40:16 +0700

    feat (app): created the app and add morgan for logging

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import express, {Express} from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 export class App {
-    constructor() {
-        const userRoute = new UserRoute();
+    server: Express;
 
+    constructor() {
+        // TODO: add routes
+
+        this.server = express();
+        this.server.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
+        this.server.use(
+            "/api",
+            express.json(),
+            express.urlencoded({extended: true}),
+            morgan("combined"),
+            // TODO: add routes
+        )
+    }
+
+    run() {
+        // TODO: initialize database
     }
 }
