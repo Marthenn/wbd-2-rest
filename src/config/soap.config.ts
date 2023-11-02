c59a040f67976ea4bed73f2ec75192513acb60bc
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-02 15:54:51 +0700

    feat (soap): created the soap config

---
+++ src/config/soap.config.ts
@@ -0,0 +1,13 @@
+const generateHost = () => {
+    return process.env.SOAP_BASE_HOST ? process.env.SOAP_BASE_HOST : "localhost";
+}
+
+const generatePort = () => {
+    return process.env.SOAP_BASE_PORT ? process.env.SOAP_BASE_PORT : "50002";
+}
+
+export const soapConfig: { host: string, port: string, key: string } = {
+    host: generateHost(),
+    port: generatePort(),
+    key: "soap"
+}
