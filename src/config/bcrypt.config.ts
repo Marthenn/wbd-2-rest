b970bd8d5482f4103bd05a8af3db0e6d268d8cf5
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-02 16:03:19 +0700

    feat (bcrypt): create bcrypt config

---
+++ src/config/bcrypt.config.ts
@@ -0,0 +1,9 @@
+import bcrypt from 'bcrypt';
+
+const generateSaltRounds = () => {
+    return process.env.SALT_ROUNDS ? +process.env.SALT_ROUNDS : 10;
+}
+
+export const bcryptConfig: {SaltRounds: number} = {
+    SaltRounds: generateSaltRounds()
+}
