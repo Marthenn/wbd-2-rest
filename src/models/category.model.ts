640d9a3b9da712b7984ef41f48745f8fe3f59ee1
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-07 08:43:33 +0700

    feat (category): made the category model

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -2,6 +2,7 @@ import {
     BaseEntity,
     Entity,
     PrimaryGeneratedColumn,
+    Column,
     OneToMany,
     JoinColumn
 } from "typeorm";
@@ -9,6 +10,14 @@ import {
 import { Book } from './book.model'
 
 @Entity()
-export class Category {
+export class Category extends BaseEntity {
+    @PrimaryGeneratedColumn({name:'category_id'})
+    categoryId: number
 
+    @Column()
+    name: string
+
+    @OneToMany(type => Book, (Book) => Book.category)
+    @JoinColumn({name:'category_id'})
+    books: Book[]
 }
