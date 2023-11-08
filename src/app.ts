1018af58b3689cacc1892b495f1ba1ced28bd451
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-08 23:11:02 +0700

    feat (app): add account routing

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express, {Express} from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { DataSource } from "typeorm";
+
+import { serverConfig } from "./config/server-config";
+import { dataConfig } from "./config/data.config";
+import { AccountRoute } from "./routes/account.route";
+import * as process from "process";
 
 export class App {
+    dataSource: DataSource;
     server: Express;
 
     constructor() {
         // TODO: add routes
+        const accountRoute = new AccountRoute();
+
+        this.dataSource = new DataSource(dataConfig);
 
         this.server = express();
         this.server.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
@@ -15,11 +25,21 @@ export class App {
             express.json(),
             express.urlencoded({extended: true}),
             morgan("combined"),
+            accountRoute.getRoute()
             // TODO: add routes
         )
     }
 
     run() {
         // TODO: initialize database
+        this.dataSource.initialize().then(() => {
+            this.server.listen(serverConfig.Port, () => {
+                console.log(
+                    `Server is running at http://localhost:${serverConfig.Port}`
+                )
+            })
+        }). catch((error) => {
+            console.log(error);
+        })
     }
 }
