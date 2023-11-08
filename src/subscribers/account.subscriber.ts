621b5e043ccd34f211e1075ce0be633d978f4410
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-11-08 21:10:43 +0700

    feat (Account): created an event subscriber to hash the password

---
+++ src/subscribers/account.subscriber.ts
@@ -0,0 +1,28 @@
+import {
+    EntitySubscriberInterface,
+    EventSubscriber,
+    InsertEvent,
+    UpdateEvent
+} from 'typeorm';
+
+import bcrypt from "bcrypt";
+
+import { bcryptConfig } from "../config/bcrypt.config";
+import { Account } from "../models/account.model";
+
+@EventSubscriber()
+export class AccountSubscriber implements EntitySubscriberInterface<Account>{
+    listenTo(): Function | string {
+        return Account;
+    }
+
+    async beforeInsert(event: InsertEvent<Account>): Promise<void> {
+        event.entity.password = await bcrypt.hash(event.entity.password, bcryptConfig.SaltRounds);
+    }
+
+    async beforeUpdate(event: UpdateEvent<Account>): Promise<void> {
+        if (event.entity && event.entity.password !== event.databaseEntity.password) {
+            event.entity.password = await bcrypt.hash(event.entity.password, bcryptConfig.SaltRounds);
+        }
+    }
+}
