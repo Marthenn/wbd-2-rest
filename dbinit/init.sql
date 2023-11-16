5bea1b5316a24054a073f29f127724816e451459
hanifmz07 <13521157@std.stei.itb.ac.id>
2023-11-16 13:47:10 +0700

    feat(db): add insert statement for sample data

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -12,7 +12,7 @@ CREATE TABLE book (
     book_id SERIAL PRIMARY KEY,
     title character varying NOT NULL,
     description character varying NOT NULL,
-    rating integer,
+    rating float, -- Rating between 0 and 5
     cover_image_directory character varying,
     duration character varying NOT NULL,
     author character varying NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE book (
 
 CREATE TABLE chapter (
     chapter_id SERIAL PRIMARY KEY,
-    chapter integer NOT NULL,
+    chapter character varying NOT NULL,
     transcript_directory character varying,
     audio_directory character varying NOT NULL,
     book_id integer,
@@ -50,3 +50,51 @@ CREATE TABLE history (
     FOREIGN KEY (uid) REFERENCES account(uid),
     FOREIGN KEY (chapter_id) REFERENCES chapter(chapter_id)
 );
+
+INSERT INTO account (is_admin, username, password, email, joined_date, expired_date)
+VALUES
+    (true, 'admin_user', 'admin_password123', 'admin@example.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '365 days'),
+    (false, 'user1', 'user1_password', 'user1@example.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '365 days'),
+    (false, 'user2', 'user2_password', 'user2@example.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '365 days');
+
+INSERT INTO book (title, description, rating, cover_image_directory, duration, author, category)
+SELECT
+    'Book ' || generate_series,
+    'Description for Book ' || generate_series,
+    (random() * (5.0 - 0.0) + 0.0)::numeric(3,1), -- Random rating between 0.0 and 5.0 with one decimal place
+    '/covers/book' || generate_series || '.jpg',
+    '00:30:00', -- Varying duration between 5 and 24 hours
+    'Author ' || (generate_series % 10 + 1),
+    CASE
+        WHEN generate_series % 3 = 0 THEN 'Fiction'
+        WHEN generate_series % 3 = 1 THEN 'Mystery'
+        ELSE 'Science Fiction'
+    END
+FROM generate_series(1, 20);
+
+INSERT INTO chapter (chapter, transcript_directory, audio_directory, book_id)
+SELECT
+    'Chapter ' || (generate_series % 5) + 1 || ': Hello World!',
+    '/transcripts/chapter' || generate_series || '.txt',
+    '/audio/chapter' || generate_series || '.mp3',
+    floor(generate_series / 5) + 1
+FROM generate_series(0, 99);
+
+INSERT INTO favorite (uid)
+SELECT
+    floor(random() * 3) + 1
+FROM generate_series(1, 50);
+
+INSERT INTO favorite_book (favorite_id, book_id)
+SELECT
+    generate_series,
+    floor(random() * 20) + 1
+FROM generate_series(1, 50);
+
+INSERT INTO history (history_id, uid, chapter_id, curr_duration)
+SELECT
+    generate_series as history_id,
+    floor(random() * 3) + 1 as uid,
+    floor(random() * 100) + 1 as chapter_id,
+    '00:30:00' as curr_duration
+FROM generate_series(1, 20);
