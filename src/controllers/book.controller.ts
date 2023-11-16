3a6e9554439eef2a4c71463684f5fcc08d0ec9e2
hanifmz07 <13521157@std.stei.itb.ac.id>
2023-11-16 14:18:53 +0700

    feat(book): add book count controller

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -7,15 +7,36 @@ export class BookController {
         return async (_req: Request, res: Response) => {
             console.log("Handling /book request"); // Add this line for debugging
             try {
+                // TODO: Add rating
                 const books = await Book.createQueryBuilder('book')
-                    .select(['book.book_id', 'book.title', 'book.rating', 'book.duration'])
+                    .select(['book.book_id', 'book.title', 'book.duration'])
                     .take(8)
                     .getMany();
                 res.status(StatusCodes.OK).json({
-                    message: false,
+                    message: ReasonPhrases.OK,
                     books,
                 });
                 console.log(books);
+            } catch (error) {
+                console.error(error);
+                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                    message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+                });
+            }
+        };
+    }
+
+    bookCount() {
+        return async (_req: Request, res: Response) => {
+            console.log("Handling /book/count request"); // Add this line for debugging
+            try {
+                const bookCount = await Book.createQueryBuilder('book')
+                    .select('COUNT(*)', 'bookCount')
+                res.status(StatusCodes.OK).json({
+                    message: ReasonPhrases.OK,
+                    bookCount,
+                });
+                console.log(bookCount);
             } catch (error) {
                 console.error(error);
                 res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
@@ -29,14 +50,15 @@ export class BookController {
         return async (req: Request, res: Response) => {
             console.log("Handling /book/:id request");
             try {
+                // TODO: Add rating
                 const book = await Book.createQueryBuilder('book')
-                    .select(['book.book_id', 'book.title', 'book.rating', 'book.duration', 'book.description', 'book.cover_image_directory'])
+                    .select(['book.book_id', 'book.title', 'book.duration', 'book.description', 'book.cover_image_directory'])
                     .where('book.book_id = :id', { 
                         id: parseInt(req.params.id) 
                     })
                     .getOne();
                 res.status(StatusCodes.OK).json({
-                    message: false,
+                    message: ReasonPhrases.OK,
                     book,
                 });
                 console.log(book);
@@ -48,5 +70,7 @@ export class BookController {
             }
         };
     }
+
+
 }
 
